ea16dba9107d3b3db9c5215e373ae698d35dca56
Mangas555 <rodrigomangas08@gmail.com>
2026-02-24 19:06:20 +0000

    Musica

--- menu.gd
+++ menu.gd
@@ -1,9 +1,22 @@
 extends Control
 
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var settings: Panel = $Settings
+
+
+func _ready():
+	main_buttons.visible = true
+	settings.visible = false
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://proje.tscn")
 func _on_options_pressed() -> void:
-	pass 
+	main_buttons.visible = false
+	settings.visible = true
+	
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_sett_pressed() -> void:
+	_ready()
