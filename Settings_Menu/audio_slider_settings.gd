33e9785ec3ad433b209a7ae448efac9e5e1a76fa
Mangas555 <rodrigomangas08@gmail.com>
2026-03-13 12:19:13 +0000

    Audio_slider

---
+++ Settings_Menu/audio_slider_settings.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+@onready var audio_name: Label = $HBoxContainer/Audio_name
+@onready var audio_name_2: Label = $HBoxContainer/Audio_name2
+@onready var h_slider: HSlider = $HBoxContainer/HSlider
+
+
+@export_enum() var bus_name 
